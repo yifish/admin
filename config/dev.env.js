a4c19a615fefd8276ee4f14c7c2cd3c54cd32407
xiaoyu <xiaoyu@chujiax.com>
2019-10-12 10:51:24 +0800

    删除文件

--- config/dev.env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  NODE_ENV: '"development"',
-  ENV_CONFIG: '"dev"',
-  BASE_URL: '"http://127.0.0.1:8001"',
-  API_BASE_URL: '"/api"', ///index.php
-  IMAGE_BASE_URL: '"/storage"'
-}
